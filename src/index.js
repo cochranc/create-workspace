f9d58cce8c893088b85da404a9a33248b0145059
Sumin Goh <gohsumin@grinnell.edu>
2020-07-25 21:45:13 -0400

    working version with all the pages and working workspace; backend stuff not included

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';;
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-//load the app
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
